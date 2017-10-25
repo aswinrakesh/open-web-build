ee551cbfce727d503dcd1a1f1c4a47bd6a67392d
aswinrakesh <33092080+aswinrakesh@users.noreply.github.com>
2017-10-25 05:50:32 -0700

    Update and rename math.c to add.c

--- add.c
+++ add.c
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main(void) {
-        int x;
+        int x,a,b;
         scanf("%d", x);
+        scanf("%d", a);
+        scanf("%d", b);
         printf("%d", x*x);
+        printf("%d", a+b);
 }
